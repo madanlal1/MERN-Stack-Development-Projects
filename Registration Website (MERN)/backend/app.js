8b29873035278f0c0495f8e74dd76d70143495c1
Madan Lal <madanlal.suther@gmail.com>
2022-06-17 21:57:56 +0500

    added registration and validation form with it's validation

--- Registration Website (MERN)/backend/app.js
+++ Registration Website (MERN)/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const env = require("dotenv").config();
+require("dotenv").config();
 
 //----------------------------------------------------------------------------------------
 
--- Registration Website (MERN)/backend/components/routes/route.js
+++ Registration Website (MERN)/backend/components/routes/route.js
@@ -1,14 +1,69 @@
 const express = require('express');
 const router = express.Router();
+require('../db/db');
+const User = require('../model/userSchema');
 
 router.get('/', (req,res) => {
     res.send("Hellow from Home...")
-    console.log('hi');
 })
 
 router.post('/register', (req,res) => {
-    res.send(req.body);
-    console.log(req.body);
+
+        //  destructuring object...
+        const {name, email, phone, password, cpassword} = req.body;
+
+
+        //  checking all fields exists...
+        if(!name || !email || !phone || !password || !cpassword) {
+            return res.status(422).json({error : "Kindly fill all fields"})
+        }
+
+        //  finding if user already exist...
+        User.findOne({email : email}).then( (userExist) => {
+            if(userExist) {
+
+                return res.status(422).json({error : "email is already exist"})
+            }
+            else {
+
+                //  creating new user...
+                const user = new User({name, email, phone, password, cpassword});
+
+
+                //  saving data in database collection named "user"...
+                user.save().then(() => {
+                    res.status(201).json({message: "User registered successfully..."})
+                }).catch (() => {
+                    res.status(500).json({error: "Failed to register"})
+                })
+
+            }
+        }).catch(err => console.log(err))
+})
+
+router.post('/signin', async (req,res) => {
+
+    try{
+        const {email, password} = req.body;
+    
+        if(!email || !password) {
+            return res.status(400).json({error: "Email or Password missing..."});
+        }
+
+        const response = await User.findOne({email: email});
+
+        if(response) {
+            res.status(200).json({message: "User login successfully"});    
+            console.log(req.body);    
+        } else {
+            res.status(400).json({error: "invalid data!"})
+        }
+
+    } catch(err) {
+        console.log(err);
+    } 
+
+
 })
 
 module.exports = router;
